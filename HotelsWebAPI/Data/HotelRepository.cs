14e9696f8b273696d6a731856605b56afc570c22
mykhailo <michaelh12r2@gmail.com>
2022-06-06 07:07:13 +0300

    add custom dto search, add custom result

--- HotelsWebAPI/Data/HotelRepository.cs
+++ HotelsWebAPI/Data/HotelRepository.cs
@@ -10,14 +10,22 @@ public class HotelRepository : IHotelRepository
     public Task<List<Hotel>> GetHotelsAsync(string name) =>
         _context.Hotels.Where(h => h.Name.Contains(name)).ToListAsync();
 
+    public async Task<List<Hotel>> GetHotelsAsync(Coordinate coordinate) =>
+        await _context.Hotels.Where(hotel =>
+            hotel.Latitude > coordinate.Latilude - 1 &&
+            hotel.Latitude < coordinate.Latilude + 1 &&
+            hotel.Longitude > coordinate.Longitude - 1 &&
+            hotel.Longitude < coordinate.Longitude + 1
+        ).ToListAsync();
+
     public async Task<Hotel> GetHotelAsync(int hotelId) =>
-        await _context.Hotels.FindAsync(new object[] { hotelId });
+        await _context.Hotels.FindAsync(new object[]{hotelId});
 
     public async Task InsertHotelAsync(Hotel hotel) => await _context.Hotels.AddAsync(hotel);
 
     public async Task UpdateHotelAsync(Hotel hotel)
     {
-        var hotelFromDb = await _context.Hotels.FindAsync(new object[] { hotel.Id });
+        var hotelFromDb = await _context.Hotels.FindAsync(new object[]{hotel.Id});
         if (hotelFromDb == null) return;
         hotelFromDb.Longitude = hotel.Longitude;
         hotelFromDb.Latitude = hotel.Latitude;
@@ -26,7 +34,7 @@ public class HotelRepository : IHotelRepository
 
     public async Task DeleteHotelAsync(int hotelId)
     {
-        var hotelFromDb = await _context.Hotels.FindAsync(new object[] { hotelId });
+        var hotelFromDb = await _context.Hotels.FindAsync(new object[]{hotelId});
         if (hotelFromDb == null) return;
         _context.Hotels.Remove(hotelFromDb);
     }
--- HotelsWebAPI/Program.cs
+++ HotelsWebAPI/Program.cs
@@ -2,7 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 builder.Services.AddDbContext<HotelDb>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("Sqlite"));
@@ -21,7 +20,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.MapGet("/hotels", async (IHotelRepository repository) =>
-    Results.Ok(await repository.GetHotelsAsync()))
+    Results.Extensions.Xml(await repository.GetHotelsAsync()))
     .Produces<List<Hotel>>(StatusCodes.Status200OK)
     .WithName("GetAllHotels")
     .WithTags("Getters");
@@ -35,32 +34,32 @@ app.MapGet("/hotels/{id}", async (int id, IHotelRepository repository) =>
     .WithTags("Getters");
 
 app.MapPost("/hotels", async ([FromBody] Hotel hotel, IHotelRepository repository) =>
-{
-    await repository.InsertHotelAsync(hotel);
-    await repository.SaveAsync();
-    return Results.Created($"/hotels/{hotel.Id}", hotel);
-})
+    {
+        await repository.InsertHotelAsync(hotel);
+        await repository.SaveAsync();
+        return Results.Created($"/hotels/{hotel.Id}", hotel);
+    })
     .Accepts<Hotel>("application/json")
     .Produces<Hotel>(StatusCodes.Status201Created)
     .WithName("CreateHotel")
     .WithTags("Creators");
 
 app.MapPut("/hotels", async ([FromBody] Hotel hotel, IHotelRepository repository) =>
-{
-    await repository.UpdateHotelAsync(hotel);
-    await repository.SaveAsync();
-    return Results.NoContent();
-})
+    {
+        await repository.UpdateHotelAsync(hotel);
+        await repository.SaveAsync();
+        return Results.NoContent();
+    })
     .Accepts<Hotel>("application/json")
     .WithName("UpdateHotel")
     .WithTags("Updaters");
 
 app.MapDelete("hotels/{id}", async (int id, IHotelRepository repository) =>
-{
-    await repository.DeleteHotelAsync(id);
-    await repository.SaveAsync();
-    return Results.NoContent();
-})
+    {
+        await repository.DeleteHotelAsync(id);
+        await repository.SaveAsync();
+        return Results.NoContent();
+    })
     .WithName("DeleteHotel")
     .WithTags("Deleters");
 
@@ -73,6 +72,13 @@ app.MapGet("/hotels/search/name/{query}",
     .Produces(StatusCodes.Status404NotFound)
     .WithName("SearchHotels")
     .WithTags("Getters")
+    .ExcludeFromDescription();
+
+app.MapGet("/hotels/search/location/{coordinate}",
+    async (Coordinate coordinate, IHotelRepository repository) =>
+        await repository.GetHotelsAsync(coordinate) is IEnumerable<Hotel> hotels
+            ? Results.Ok(hotels)
+            : Results.NotFound(Array.Empty<Hotel>()))
     .ExcludeFromDescription();
 
 app.UseHttpsRedirection();
